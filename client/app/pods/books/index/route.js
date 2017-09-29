aa22dd293ab2cc1f71a94f8739b3922efd9a85c0
Dan Steshenko <luciferburn26@gmail.com>
2017-09-29 23:26:23 +0300

    Improve seaching by title

--- client/app/pods/books/index/route.js
+++ client/app/pods/books/index/route.js
@@ -1,10 +1,33 @@
 import Ember from 'ember';
+import FindQuery from 'ember-emberfire-find-query/mixins/find-query';
 
-export default Ember.Route.extend({
+const {
+  Route,
+  RSVP
+} = Ember;
 
-  model() {
-    return this.store.findAll('book', {
-      orderBy: 'title'
-    });
+export default Route.extend(FindQuery, {
+  queryParams: {
+    search: {
+      refreshModel: true
+    }
+  },
+
+  model({
+    search
+  }) {
+    if (search) {
+      return new RSVP.Promise((resolve) => {
+        this.filterContains(this.store, 'book', {
+          'title': search
+        }, function (books) {
+          resolve(books);
+        })
+      })
+    } else {
+      return this.store.query('book', {
+        orderBy: 'title'
+      })
+    }
   }
 });
