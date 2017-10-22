c53679ac2c0d35af024001289a1f47f815adf08e
Dan Steshenko <dan.steshenko@gmail.com>
2017-10-22 13:59:15 +0300

    Change styling for book-cover

--- client/app/pods/components/book-cover/component.js
+++ client/app/pods/components/book-cover/component.js
@@ -1,18 +1,7 @@
 import Component from '@ember/component';
-import { computed, get } from '@ember/object';
-import { htmlSafe } from '@ember/string';
 
 export default Component.extend({
-  attributeBindings: ['style'],
   localClassNames: ['book-cover'],
 
-  book: null,
-  isBlank: false,
-
-  coverBg: computed('book.coverUrl', {
-    get() {
-      const coverUrl = get(this, 'book.coverUrl');
-      return htmlSafe(`background-image:url("${coverUrl}")`);
-    }
-  })
+  coverUrl: null
 });
