cb6758e1fe353e926e82fd845d2673482683e038
Dan Steshenko <dan.steshenko@gmail.com>
2017-10-27 23:20:32 +0300

    Fix author portrait markup for author list and author view

--- client/app/pods/components/author-portrait/component.js
+++ client/app/pods/components/author-portrait/component.js
@@ -1,5 +1,24 @@
 import Component from '@ember/component';
+import {
+  computed,
+  get
+} from '@ember/object'
+import {
+  htmlSafe
+} from '@ember/string'
 
 export default Component.extend({
-  localClassNames: ['author-portrait']
+  tagName: '',
+
+  portraitUrl: null,
+
+  portraitBg: computed('portraitUrl', {
+    get() {
+      const portraitUrl = get(this, 'portraitUrl');
+      const placeholder = '/img/avatar-placeholder.png'
+      const portraitBg = portraitUrl ? portraitUrl : placeholder;
+
+      return htmlSafe(`background-image: url(${portraitBg})`)
+    }
+  })
 });
