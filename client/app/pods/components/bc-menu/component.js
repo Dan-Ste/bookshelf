109370ab30138cc9a78d49fb1fd8dac367c5367c
Dan Steshenko <dan.steshenko@gmail.com>
2017-10-29 16:39:41 +0300

    Move some layout to components

---
+++ client/app/pods/components/bc-menu/component.js
@@ -0,0 +1,9 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  localClassNames: ['menu'],
+  classNames: ['ui left vertical menu fixed'],
+
+  bookshelves: null,
+  user: null
+});
