c1f5e92714fc818e2828e897f62f4101c7308d80
Dan Steshenko <dan.steshenko@gmail.com>
2017-11-05 22:07:34 +0300

    Ask for author deletion

--- client/app/pods/components/author-view/component.js
+++ client/app/pods/components/author-view/component.js
@@ -1,7 +1,20 @@
 import Component from '@ember/component';
+import {
+  set
+} from '@ember/object'
 
 export default Component.extend({
+  localClassNames: ['author-view'],
+
   author: null,
 
-  localClassNames: ['author-view']
+  isShowAuthorDeleteConfirm: false,
+
+  openAuthorDeleteConfirm() {
+    set(this, 'isShowAuthorDeleteConfirm', true)
+  },
+
+  closeAuthorDeleteConfirm() {
+    set(this, 'isShowAuthorDeleteConfirm', false)
+  }
 });
