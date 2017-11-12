b2d63ed71d4aa0eeecff3f7705333b3fca272eea
Dan Steshenko <dan.steshenko@gmail.com>
2017-11-12 21:35:36 +0300

    Add routes animations

--- client/app/transitions.js
+++ client/app/transitions.js
@@ -5,7 +5,7 @@ export default function () {
   //     this.toRoute('people.detail'),
   //     this.use('toLeft'),
   //     this.reverse('toRight')
-  //   );
+  //   )
 
   this.transition(
     this.hasClass('fade'),
@@ -16,5 +16,83 @@ export default function () {
     this.reverse('fade', {
       duration: 100
     })
-  );
+  )
+
+  // Books transitions
+  this.transition(
+    this.fromRoute('books.index'),
+    this.toRoute('books.book'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  )
+
+  this.transition(
+    this.toRoute('books.index'),
+    this.fromRoute('books.new'),
+    this.use('toRight'),
+    this.reverse('toLeft')
+  )
+
+  this.transition(
+    this.toRoute('books.book.index'),
+    this.fromRoute('books.book.edit'),
+    this.use('toRight'),
+    this.reverse('toLeft')
+  )
+
+  // Authors transitions
+  this.transition(
+    this.toRoute('authors.index'),
+    this.fromRoute('authors.author'),
+    this.use('toRight'),
+    this.reverse('toLeft')
+  )
+
+  this.transition(
+    this.toRoute('authors.index'),
+    this.fromRoute('authors.new'),
+    this.use('toRight'),
+    this.reverse('toLeft')
+  )
+
+  this.transition(
+    this.toRoute('authors.author.index'),
+    this.fromRoute('authors.author.edit'),
+    this.use('toRight'),
+    this.reverse('toLeft')
+  )
+
+  // Top level routes
+  this.transition(
+    this.toRoute('authors'),
+    this.fromRoute('books'),
+    this.use('fade', {
+      duration: 70
+    }),
+    this.reverse('fade', {
+      duration: 70
+    })
+  )
+
+  this.transition(
+    this.toRoute('authors'),
+    this.fromRoute('bookshelf'),
+    this.use('fade', {
+      duration: 70
+    }),
+    this.reverse('fade', {
+      duration: 70
+    })
+  )
+
+  this.transition(
+    this.toRoute('books'),
+    this.fromRoute('bookshelf'),
+    this.use('fade', {
+      duration: 70
+    }),
+    this.reverse('fade', {
+      duration: 70
+    })
+  )
 }
