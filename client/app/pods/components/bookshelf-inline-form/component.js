0fca0895b1f72ab28d6f62505a9eeaf13b04d53d
Dan Steshenko <dan.steshenko@gmail.com>
2017-11-15 22:59:18 +0300

    Clean up styles

--- client/app/pods/components/bookshelf-inline-form/component.js
+++ client/app/pods/components/bookshelf-inline-form/component.js
@@ -2,13 +2,13 @@ import Component from '@ember/component'
 import {
   get
 } from '@ember/object'
+import BookshelfColors from '../../../utils/bookshelf-colors'
 
 export default Component.extend({
   localClassNames: ['bookshelf-inline-form'],
 
   bookshelf: null,
-
-  colors: ['#B03060', '#FE9A76', '#FFD700', '#32CD32', '#008080', '#EE82EE', '#FF1493', '#B413EC'],
+  BookshelfColors,
 
   willDestroyElement() {
     this._super(...arguments)
